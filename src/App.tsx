2259ab3aca6baac863c91eddf537649f458eddc3
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-05-21 14:00:44 -0300

    feat: init repository

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Nativa</h1>
+      <p>A mini ecommerce.</p>
+    </div>
+  );
+};
+
+export default App;
