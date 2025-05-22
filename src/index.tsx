8d62cfcc8d443c3b5b47dce8c42393297485243f
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-05-22 09:05:35 -0300

    feat: react router added

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import App from './App';
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App
+  }
+]);
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>,
   );
 }
