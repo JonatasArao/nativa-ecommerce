8633b51d8d1f62eea3dfd717940b5954d2bd8450
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-06-03 10:48:19 -0300

    style: minior style changes

--- src/components/atoms/elements/button.tsx
+++ src/components/atoms/elements/button.tsx
@@ -3,6 +3,7 @@ import { styled } from "@linaria/react";
 export const ActionButton = styled.button`
   border: 1px solid #059669;
   background-color: #FFF;
+  color: #000;
   padding: 0.65rem 0;
   border-radius: 2rem;
   font-weight: 600;
--- src/components/organisms/product-list/product-list.styled.tsx
+++ src/components/organisms/product-list/product-list.styled.tsx
@@ -3,6 +3,7 @@ import { styled } from "@linaria/react";
 export const ProductListWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  padding: 1rem 0;
   gap: 1rem;
   @media (min-width: 768px) {
     grid-template-columns: repeat(4, 1fr);
